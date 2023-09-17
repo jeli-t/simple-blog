efcd430da423069b69d1ecf362a9e048f3be8095
jeli-t <jelitotomek@gmail.com>
2023-09-17 19:37:37 +0200

    add layout

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Simple blog</title>
+</head>
+<body>
+    <nav class="navbar">
+        <h1>Simple</h1>
+    </nav>
+    <div class="wrapper">
+        <div class="content">
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+            <h1>Content</h1>
+        </div>
+        <div class="footer">
+            <h2>Footer</h2>
+        </div>
+    </div>
+</body>
+</html>
