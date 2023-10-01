ab9224856f5e9049e5b0d127ac54e8553be9177d
jeli-t <jelitotomek@gmail.com>
2023-10-01 19:19:34 +0200

    add responsive navbar

--- index.php
+++ index.php
@@ -4,12 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
-    <title>Simple blog</title>
+    <title>Simple CMS</title>
 </head>
 <body>
-    <nav class="navbar">
-        <h1>Simple</h1>
+
+    <nav class="nav">
+        <input type="checkbox" id="nav-check">
+        <div class="nav-header">
+            <div class="nav-title">
+                Simple CMS
+            </div>
+        </div>
+        <div class="nav-btn">
+            <label for="nav-check">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+        </div>
+        <div class="nav-links">
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+        </div>
     </nav>
+
     <div class="wrapper">
         <div class="content">
             <h1>Content</h1>
@@ -45,9 +63,11 @@
             <h1>Content</h1>
             <h1>Content</h1>
         </div>
+
         <div class="footer">
             <h2>Footer</h2>
         </div>
+
     </div>
 </body>
 </html>
