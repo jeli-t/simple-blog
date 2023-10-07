83662e8c225692824464bb0ebe10b1801b582dcc
jeli-t <jelitotomek@gmail.com>
2023-10-07 15:38:23 +0200

    add 404 page style

--- page/404.php
+++ page/404.php
@@ -1,2 +1,4 @@
-<h1>404</h1>
-<h2>Page not found</h2>
+<div class="center" style="background-color: var(--primary3)">
+    <h1 style="font-size: 80px; color: var(--primary1)">404</h1>
+    <h1 style="color: var(--primary1)">Page not found</h1>
+</div>
