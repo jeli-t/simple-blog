4c99f98970322bf0b816a0048682aafaae0493ac
jeli-t <jelitotomek@gmail.com>
2023-10-08 20:05:40 +0200

    fetching data for home page

--- component/user.php
+++ component/user.php
@@ -17,4 +17,19 @@ function getUserBlogId( $id ) {
 
 function getCurentUserBlogId() { return getUserBlogId( $_SESSION[ 'id' ] ); }
 
+
+function getAuthor( $id_blog ) {
+
+    global $connect;
+
+    $query = 'SELECT `nick` FROM `user` WHERE `id` = ' . $id_blog + 1;
+    $result = mysqli_query( $connect, $query );
+    if( $result ) {
+        $row = mysqli_fetch_assoc( $result );
+        return $row[ 'nick' ];
+    } else {
+        return 404;
+    }
+
+}
 ?>
--- page/home.php
+++ page/home.php
@@ -1,34 +1,21 @@
 <div class='content'>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
-    <h1>Content</h1>
+
+<?php
+    $query = 'SELECT * FROM `post`';
+    $result = mysqli_query( $connect, $query );
+
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $author = getAuthor($row['id_blog']);
+            echo('<H3>Author: ' . $author . '</H3>');
+            echo('<H1>Title: ' . $row['title'] . '</H1>');
+            echo('<H2>Description: ' . $row['short_desc'] . '</H2>');
+        }
+
+    } else {
+        echo('<H1>There are no posts yet</H1>');
+    }
+
+?>
+
 </div>
